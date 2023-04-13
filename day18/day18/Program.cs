270edcc55d83d5286b63aca64578ee62809c5da9
蒋更杰了 <79917398+jiangjikang@users.noreply.github.com>
2023-04-13 21:54:37 +0800

    Add files via upload

---
+++ day18/day18/Program.cs
@@ -0,0 +1,308 @@
+﻿namespace day18;
+
+//引用参数 -> 值类型
+//class Program
+//{
+//    
+//    static void Main(string[] args)
+//    {
+//        int y = 1;
+//        IWantSideEffect(ref y);
+//        Console.WriteLine(y);
+//    }
+
+//    static void IWantSideEffect(ref int x)
+//    {
+//        x += 100;
+//    }
+//}
+
+
+
+
+
+//------------------------------------------------------------------------------------------------------
+
+
+//引用参数 -> 引用类型
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        var outterStu = new Student() { Name = "Tim" };
+//        Console.WriteLine("HashCode={0}, Name={1}", outterStu.GetHashCode(), outterStu.Name);
+//        Console.WriteLine("-----------------");
+//        IWantSideEffect(ref outterStu);
+//        Console.WriteLine("HashCode={0}, Name={1}", outterStu.GetHashCode(), outterStu.Name);
+//    }
+
+//    static void IWantSideEffect(ref Student stu)
+//    {
+//        stu = new Student() { Name = "Tom" };
+//        Console.WriteLine("HashCode={0}, Name={1}", stu.GetHashCode(), stu.Name);
+//    }
+//}
+
+//class Student
+//{
+//    public string Name { get; set; }
+//}
+
+
+
+
+
+
+
+//------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+//输出参数 -> 值类型
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine("Please input first number:");
+//        var arg1 = Console.ReadLine();
+//        double x = 0;
+//        if (double.TryParse(arg1, out x) == false)
+//        {
+//            Console.WriteLine("Input error!");
+//            return;
+//        }
+
+//        Console.WriteLine("Please input second number:");
+//        var arg2 = Console.ReadLine();
+//        double y = 0;
+//        if (double.TryParse(arg2, out y) == false)
+//        {
+//            Console.WriteLine("Input error!");
+//            return;
+//        }
+
+//        double z = x + y;
+//        Console.WriteLine(z);
+//    }
+//}
+
+
+
+
+
+
+
+//------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+//自己实现了带有输出参数的 TryParse
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        double x = 0;
+//        if (DoubleParser.TryParse("aa", out x))
+//        {
+//            Console.WriteLine(x);
+//        }
+//    }
+//}
+
+//class DoubleParser
+//{
+//    public static bool TryParse(string input, out double result)
+//    {
+//        try
+//        {
+//            result = double.Parse(input);
+//            return true;
+//        }
+//        catch
+//        {
+//            result = 0;
+//            return false;
+//        }
+//    }
+//}
+
+
+
+
+
+
+
+
+//------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+//输出参数 -> 引用类型
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Student stu = null;
+//        if (StudentFactory.Create("Tim", 34, out stu))
+//        {
+//            Console.WriteLine("Student {0}, age is {1}", stu.Name, stu.Age);
+//        }
+//    }
+//}
+
+//class Student
+//{
+//    public int Age { get; set; }
+//    public string Name { get; set; }
+//}
+
+//class StudentFactory
+//{
+//    public static bool Create(string stuName, int stuAge, out Student result)
+//    {
+//        result = null;
+//        if (string.IsNullOrEmpty(stuName))
+//        {
+//            return false;
+//        }
+
+//        if (stuAge < 20 || stuAge > 80)
+//        {
+//            return false;
+//        }
+
+//        result = new Student() { Name = stuName, Age = stuAge };
+//        return true;
+//    }
+//}
+
+
+
+
+
+//------------------------------------------------------------------------------------------------------
+
+
+
+
+//数组参数 params
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        int result = CalculateSum(1,2,3);
+//        Console.WriteLine(result);
+
+
+//        string str = "jiang;ji.kang";
+//        string[] strArray = str.Split(';','.');
+//        foreach (var item in strArray)
+//        {
+//            Console.WriteLine(item);
+//        }
+//    }
+
+//    static int CalculateSum(params int[] intArray)
+//    {
+//        int sum = 0;
+//        foreach (var item in intArray)
+//        {
+//            sum += item;
+//        }
+//        return sum;
+//    }
+//}
+
+
+
+
+
+//------------------------------------------------------------------------------------------------------
+
+
+
+
+//具名参数: 提高代码的可读性、不受参数列表顺序的约束
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        PrintInfo(name:"jiang", age:23);
+//    }
+
+//    static void PrintInfo(string name, int age)
+//    {
+//        Console.WriteLine("Hello {0} you are {1}", name, age);
+//    }
+//}
+
+
+
+
+
+//------------------------------------------------------------------------------------------------------
+
+
+
+
+
+//可选参数: 有默认值
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        PrintInfo();
+//    }
+
+//    static void PrintInfo(string name = "Tim", int age = 34)
+//    {
+//        Console.WriteLine("Hello {0} you are {1}", name, age);
+//    }
+//}
+
+
+
+
+
+
+//------------------------------------------------------------------------------------------------------
+
+
+
+
+
+//扩展方法:
+//方法必需是公有、静态的，即被public static所修饰
+//必需是形参列表中的第一个，由this修饰
+//必需由一个静态类（一般类名为SomeTypeExtension）来统一收纳对SomeType类型的扩展方法
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        double x = 3.1415926;
+//        double y = x.Round(4);
+//        Console.WriteLine(y);
+//    }
+//}
+
+
+//static class DoubleExtension
+//{
+//    public static double Round(this double input, int digits)
+//    {
+//        double result = Math.Round(input, digits);
+//        return result;
+//    }
+//}
---
+++ day19/day19/Program.cs
@@ -0,0 +1,292 @@
+﻿namespace day19;
+
+//Action 和 Func委托
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Calculator calculator = new Calculator();
+//        Action action = new Action(calculator.Report);
+//        action();
+//        Func<int, int, int> func1 = new Func<int, int, int>(calculator.Add);
+//        Func<int, int, int> func2 = new Func<int, int, int>(calculator.Sub);
+
+//        int y1 = func1(100, 200);
+
+//        int y2 = func2(500, 200);
+//        Console.WriteLine(y1);
+//        Console.WriteLine(y2);
+
+
+//    }
+//}
+
+//class Calculator
+//{
+//    public void Report()
+//    {
+//        Console.WriteLine("I have three Methods");
+//    }
+
+//    public int Add(int a, int b)
+//    {
+//        return a + b;
+//    }
+
+//    public int Sub(int a, int b)
+//    {
+//        return a - b;
+//    }
+//}
+
+
+
+
+
+
+
+//------------------------------------------------------------------------------------------------------
+
+
+
+
+//自定义委托（delegate）
+//•委托是一种类（class），类是数据类型所以委托也是一种数据类型
+//•它的声名方式与一般的类不同，主要是为了照顾可读性和C/C++传统
+//•注意声明委托的位置
+//•避免写错地方结果声明成嵌套类型
+//•委托与所封装的方法必需“类型兼容〞
+//public delegate double Calc(double x, double y);
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Calculator calculator = new Calculator();
+//        Calc calc1 = new Calc(calculator.Add);
+//        Calc calc2 = new Calc(calculator.Sub);
+//        Calc calc3 = new Calc(calculator.Mul);
+//        Calc calc4 = new Calc(calculator.Div);
+
+//        Console.WriteLine(calc1(1000, 100));
+//        Console.WriteLine(calc2(1000, 100));
+//        Console.WriteLine(calc3(1000, 100));
+//        Console.WriteLine(calc4(1000, 100));
+//    }
+//}
+
+
+//class Calculator
+//{
+//    public double Add(double a, double b)
+//    {
+//        return a + b;
+//    }
+
+//    public double Sub(double a, double b)
+//    {
+//        return a - b;
+//    }
+
+//    public double Mul(double x, double y)
+//    {
+//        return x * y;
+//    }
+
+//    public double Div(double x, double y)
+//    {
+//        return x / y;
+//    }
+//}
+
+
+
+
+
+
+
+
+//------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+//模版方法
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        ProductFactory productFactory = new ProductFactory();
+//        WrapFactory wrapFactory = new WrapFactory();
+
+//        Func<Product> func1 = new Func<Product>(productFactory.MakePizza);
+//        Func<Product> func2 = new Func<Product>(productFactory.MakeToyCar);
+
+//        Box box1 = wrapFactory.Wraproduct(func1);
+//        Box box2 = wrapFactory.Wraproduct(func2);
+
+//        Console.WriteLine(box1.Product.Name);
+//        Console.WriteLine(box2.Product.Name);
+//    }
+//}
+
+
+//class Product
+//{
+//    public string Name { get; set; }
+//}
+
+
+//class Box
+//{
+//    public Product Product { get; set; }
+//}
+
+
+//class WrapFactory
+//{
+//    public Box Wraproduct(Func<Product> getProduct)
+//    {
+//        Box box = new Box();
+//        Product product = getProduct.Invoke();
+//        box.Product = product;
+//        return box;
+//    }
+//}
+
+
+//class ProductFactory
+//{
+//    public Product MakePizza()
+//    {
+//        Product product = new Product();
+//        product.Name = "Pizza";
+//        return product;
+//    }
+
+//    public Product MakeToyCar()
+//    {
+//        Product product = new Product();
+//        product.Name = "ToyCar";
+//        return product;
+//    }
+//}
+
+
+
+
+
+
+
+
+//------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+//回调方法
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        ProductFactory productFactory = new ProductFactory();
+//        WrapFactory wrapFactory = new WrapFactory();
+
+//        Func<Product> func1 = new Func<Product>(productFactory.MakePizza);
+//        Func<Product> func2 = new Func<Product>(productFactory.MakeToyCar);
+
+//        Logger logger = new Logger();
+//        Action<Product> log = new Action<Product>(logger.Log);
+
+//        Box box1 = wrapFactory.Wraproduct(func1, log);
+//        Box box2 = wrapFactory.Wraproduct(func2, log);
+
+//        Console.WriteLine(box1.Product.Name);
+//        Console.WriteLine(box2.Product.Name);
+//    }
+//}
+
+
+//class Logger
+//{
+//    public void Log(Product product)
+//    {
+//        Console.WriteLine("Product {0} created at {1}. Price is {2}.", product.Name, DateTime.UtcNow, product.Price);
+//    }
+//}
+
+
+//class Product
+//{
+//    public string Name { get; set; }
+//    public double Price { get; set; }
+//}
+
+
+//class Box
+//{
+//    public Product Product { get; set; }
+//}
+
+
+//class WrapFactory
+//{
+//    public Box Wraproduct(Func<Product> getProduct, Action<Product> logCallback)
+//    {
+//        Box box = new Box();
+//        Product product = getProduct.Invoke();
+//        if (product.Price >= 50)
+//        {
+//            logCallback(product);
+//        }
+//        box.Product = product;
+//        return box;
+//    }
+//}
+
+
+//class ProductFactory
+//{
+//    public Product MakePizza()
+//    {
+//        Product product = new Product();
+//        product.Name = "Pizza";
+//        product.Price = 12;
+//        return product;
+//    }
+
+//    public Product MakeToyCar()
+//    {
+//        Product product = new Product();
+//        product.Name = "ToyCar";
+//        product.Price = 100;
+//        return product;
+//    }
+//}
+
+
+
+
+
+
+
+
+
+//------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
